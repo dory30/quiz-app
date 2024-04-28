7183510cf11458a39df051e0cb02a815f5e1eb06
dory30 <martinedorianeingrid30@gmail.com>
2024-04-29 00:27:13 +0100

    one

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import './App.css'; // Assurez-vous de créer un fichier App.css pour le style
 function App() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
-  const [selectedOption, setSelectedOption] = useState("");
-  const [showScore, setShowScore] = useState (false);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [showScore, setShowScore] = useState(false);
   const [isAnswered, setIsAnswered] = useState(false);
   const [isCorrect, setIsCorrect] = useState(false);
-  
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
+
 
   const retryQuiz = () => {
     // Réinitialiser l'état du quiz
@@ -24,55 +25,78 @@ function App() {
     setSelectedOption(option);
     setIsAnswered(true);
     setIsCorrect(isAnswerCorrect);
-    if (isAnswerCorrect) {
-      setScore(score + 1);
-    }
+    //if (isAnswerCorrect) {
+    // setScore(score + 1);
+    //}
   };
 
   const handleSubmitClick = () => {
-    if (!isAnswered) return; // Ajoutez une logique pour empêcher de passer à la prochaine question sans réponse
-    
-    const nextQuestionIndex = currentQuestionIndex + 1;
-    if (nextQuestionIndex < quizData.length) {
-      setCurrentQuestionIndex(nextQuestionIndex);
-    } else {
-      setShowScore(true);
-    }
-    setIsAnswered(false); // Réinitialiser pour la prochaine question
-  };
 
+    if (!isCorrect) {
+      setTimeout(() => {
+        if (currentQuestionIndex < quizData.length - 1) {
+          setCurrentQuestionIndex(currentQuestionIndex + 1);
+          setIsAnswered(false);
+          setSelectedOption("");
+          setIsCorrect(null);
+        } else {
+          setShowScore(true);
+        }
+      }, 2000); // Attendre 2 secondes
+    } else {
+      // Si la réponse est correcte, passez immédiatement à la prochaine question
+      if (currentQuestionIndex < quizData.length - 1) {
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
+        setIsAnswered(false);
+        setSelectedOption("");
+        setIsCorrect(null);
+      } else {
+        setShowScore(true);
+      }
+    }
+
+   };
+
+
+  
   return (
     <div className="App">
-       {showScore ? (
+      {showScore ? (
         <ResultScreen score={score} questions={quizData} onRetry={retryQuiz} />
       ) : (
-      <div className="quiz-container">
-        <h1>Quiz App</h1>
-        <div className="question-section">
-          <div className="question-count">
-            <span>Question {currentQuestionIndex + 1}</span>/{quizData.length}
+        <div className="quiz-container">
+          <h1>Quiz App</h1>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentQuestionIndex + 1}</span>/{quizData.length}
+            </div>
+            <div className="question-text">
+              {quizData[currentQuestionIndex].question}
+            </div>
           </div>
-          <div className="question-text">
-            {quizData[currentQuestionIndex].question}
+
+          <div className="option-section">
+            {quizData[currentQuestionIndex].options.map((option, index) => (
+              <button
+                key={index}
+                className={`option-button 
+        ${selectedOption === option ? (isAnswerCorrect ? "correct" : "incorrect") : ""}
+        ${isAnswerCorrect === false && quizData[currentQuestionIndex].answer === option ? "correct-answer-blink" : ""}`}
+                onClick={() => handleOptionClick(option)}
+                disabled={isAnswered}
+              >
+                {option}
+              </button>
+            ))}
           </div>
+
+
+
+          <button className="submit-button" onClick={handleSubmitClick}>
+            Next
+          </button>
         </div>
-        <div className="option-section">
-          {quizData[currentQuestionIndex].options.map((option, index) => (
-            <button
-              key={index}
-              className={`option-button ${selectedOption === option ? (isCorrect ? "correct" : "incorrect") : ""}`}
-              onClick={() => handleOptionClick(option)}
-              disabled={isAnswered}
-            >
-              {option}
-            </button>
-          ))}
-        </div>
-        <button className="submit-button" onClick={handleSubmitClick}>
-          Next
-        </button>
-      </div>
-       )}
+      )}
     </div>
   );
 }
--- src/quizData.js
+++ src/quizData.js
@@ -1,31 +1,64 @@
-const  quizData = [
-  {
-    question: "Quelle est la capitale du Japon ?",
-    options: ["Tokyo", "Kyoto", "Osaka", "Nagoya"],
-    answer: "Tokyo"
-  },
-  {
-    question: "Quelle est la capitale de l'Allemagne ?",
-    options: ["Berlin", "Munich", "Hambourg", "Cologne"],
-    answer: "Berlin"
-  },
-  {
-    question: "Quelle est la capitale de l'Espagne ?",
-    options: ["Madrid", "Barcelone", "Séville", "Valence"],
-    answer: "Madrid"
-  },
-  {
-    question: "Quelle est la capitale du Canada ?",
-    options: ["Ottawa", "Toronto", "Montréal", "Vancouver"],
-    answer: "Ottawa"
-  },
-  {
-    question: "Quelle est la capitale du Brésil ?",
-    options: ["Brasilia", "Rio de Janeiro", "São Paulo", "Salvador"],
-    answer: "Brasilia"
-  },
-    
-  ];
-  
-  export default  quizData;
-  
+const quizData = [
+{
+    question: "Who wrote 'Les Misérables'?",
+    options: ["Victor Hugo", "Albert Camus", "Voltaire", "Émile Zola"],
+    answer: "Victor Hugo"
+},
+
+{
+    question: "What is the chemical formula for water?",
+    options: ["H2O", "CO2", "O2", "H2SO4"],
+    answer: "H2O"
+},
+
+{
+    question: "Which city is the capital of the United States?",
+    options: ["New York", "Washington D.C.", "Los Angeles", "Chicago"],
+    answer: "Washington D.C."
+},
+
+{
+    question: "Which chemical element has the symbol 'Au'?",
+    options: ["Silver", "Lead", "Gold", "Aluminum"],
+    answer: "Gold"
+},
+
+{
+    question: "What year did the first man walk on the moon?",
+    options: ["1969", "1972", "1965", "1970"],
+    answer: "1969"
+},
+
+{
+    question: "Which country is the largest producer of coffee in the world?",
+    options: ["Brazil", "Vietnam", "Colombia", "Ethiopia"],
+    answer: "Brazil"
+},
+
+{
+    question: "In which organ of the human body is the retina found?",
+    options: ["Heart", "Lung", "Eyes", "Liver"],
+    answer: "Eyes"
+},
+
+{
+    question: "Who is the author of the theory of relativity?",
+    options: ["Isaac Newton", "Albert Einstein", "Galileo", "Stephen Hawking"],
+    answer: "Albert Einstein"
+},
+
+{
+    question: "What is the most played sport in the world?",
+    options: ["Basketball", "Tennis", "Soccer", "Golf"],
+    answer: "Soccer"
+},
+
+{
+    question: "What is the longest river in the world?",
+    options: ["Amazon", "Nile", "Yangtze", "Mississippi"],
+    answer: "Amazon"
+}
+
+];
+
+export default quizData;
